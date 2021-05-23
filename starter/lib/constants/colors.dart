aa6fa4f2c0b67fc924c592a6d08414fe7c256976
Leandro Pontes Berleze <leandropberleze@gmail.com>
2021-05-23 13:53:50 -0300

    Feat: add custom text atom

---
+++ starter/lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+const kPrimaryColor = Color(0xFFFF7043);
+const kPrimaryColorLight = Color(0xFFFFA270);
+const kPrimaryColorDark = Color(0xFFC63F17);
+const kPrimaryTextColor = Color(0xFF000000);
+const kCanvasColor = Color(0xFFF6F6F6);
+const kTitleColor = Color(0xFF010101);
+const kTextGreyDark = Color(0xFF6E6E6E);
