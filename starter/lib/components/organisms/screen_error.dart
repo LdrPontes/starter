e8da0a1d8d25f15a528d1d526a63b3fea3194c33
Leandro Pontes Berleze <leandropberleze@gmail.com>
2021-05-29 16:32:59 -0300

    Refactor: refactor screen_error

--- starter/lib/components/organisms/screen_error.dart
+++ starter/lib/components/organisms/screen_error.dart
@@ -15,43 +15,45 @@ class ScreenError extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset(
-            'assets/svg/warning.svg',
-            height: 100,
-            width: 100,
-          ),
-          SizedBox(
-            height: 24,
-          ),
-          CustomText(_title, style: CustomTextStyle.TITLE_MEDIUM),
-          SizedBox(
-            height: 8,
-          ),
-          CustomText(
-            _message,
-            style: CustomTextStyle.REGULAR,
-            wrap: true,
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(
-            height: 24,
-          ),
-          MaterialButton(
-            onPressed: _onPressTryAgain,
-            color: kPrimaryColor,
-            child: CustomText(
-              'Try Again',
-              style: CustomTextStyle.REGULAR,
-              color: kWhite,
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'assets/svg/warning.svg',
+              height: 100,
+              width: 100,
             ),
-          )
-        ],
+            SizedBox(
+              height: 24,
+            ),
+            CustomText(_title, style: CustomTextStyle.TITLE_MEDIUM),
+            SizedBox(
+              height: 8,
+            ),
+            CustomText(
+              _message,
+              style: CustomTextStyle.REGULAR,
+              wrap: true,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            MaterialButton(
+              onPressed: _onPressTryAgain,
+              color: kPrimaryColor,
+              child: CustomText(
+                'Try Again',
+                style: CustomTextStyle.REGULAR,
+                color: kWhite,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
